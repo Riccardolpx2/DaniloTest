a0e4349b4c053d0a8ecba8b9cfa8942696efe33f
riccardo <r.laporta6@studenti.unisa.it>
2026-06-04 15:38:22 +0200

    modifiche data mesage

--- src/main/java/shared/model/protocol/Message.java
+++ src/main/java/shared/model/protocol/Message.java
@@ -4,8 +4,27 @@ import java.io.Serializable;
 
 public class Message implements Serializable {
     private MessageType msgType;
-    private String playerID;
+    private String playerID; //username
     private Object payload;
+
+    public Message(MessageType msgType, String playerID, Object payload) {
+        this.msgType = msgType;
+        this.playerID = playerID;
+        this.payload = payload;
+    }
+
+    public MessageType getMsgType() {
+        return msgType;
+    }
+
+    public String getPlayerID() {
+        return playerID;
+    }
+
+    public Object getPayload() {
+        return payload;
+    }
+
 
     // TODO: Da continuare con la struttura
 }
